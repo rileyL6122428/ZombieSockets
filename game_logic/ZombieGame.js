6c6f5d5fbc428772acfdfd999801ae66eb76bae8
Riley Littlefield <rileylittlefield@ymail.com>
2016-08-12 18:48:43 -0400

    adds random starting zombie

--- game_logic/ZombieGame.js
+++ game_logic/ZombieGame.js
@@ -1,15 +1,19 @@
 // 'use strict'
-
+var SocketShuffler = require('./utils/socket_shuffler.js');
 var Boundary = require('./boundary2.js');
 var Player = require('./Player.js');
 var HumanPlayer = require('./Human.js');
 var ZombiePlayer = require('./Zombie.js');
 
 function ZombieTestDemo(sock1, sock2, io) {
-  this._players = [new ZombiePlayer(sock1), new HumanPlayer(sock2)];
+  var shuffledSocks = SocketShuffler.shuffledSockets([sock1, sock2]);
+  this._players = [new ZombiePlayer(shuffledSocks[0]), new HumanPlayer(shuffledSocks[1])];
+  this._zombies = this._players[0];
+  this._humans = this._players.slice(1);
+
   this.boundary = new Boundary(500, 500);
   this._initSockets(io);
-  this.setupPositionUpdateCallback(io);
+  this.setupClientUpdateCallback(io);
 }
 
 ZombieTestDemo.prototype._initSockets = function (io) {
@@ -31,7 +35,7 @@ ZombieTestDemo.prototype.playerPositions = function () {
   return positions;
 };
 
-ZombieTestDemo.prototype.setupPositionUpdateCallback = function (io) {
+ZombieTestDemo.prototype.setupClientUpdateCallback = function (io) {
   var that = this;
   setInterval(function () {
     io.emit('position update', that.playerPositions());
